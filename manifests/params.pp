f18e7a0ea56e1193d61920cd1bd3d7610e7d4112
Eric Shamow <eric.shamow@gmail.com>
2014-11-18 09:54:14 -0800

    Remove trailing slashes

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,10 +7,10 @@ class consul::params {
   case $::osfamily {
     'RedHat', 'Amazon': {
       $package_name         = 'consul'
-      $package_location     = 'https://s3.amazonaws.com/opsrealist_pkgs/RPMs/x86_64/'
+      $package_location     = 'https://s3.amazonaws.com/opsrealist_pkgs/RPMs/x86_64'
       $package_version      = '0.4.1-1'
       $web_package_name     = 'consul-web'
-      $web_package_location = 'https://s3.amazonaws.com/opsrealist_pkgs/RPMs/noarch/'
+      $web_package_location = 'https://s3.amazonaws.com/opsrealist_pkgs/RPMs/noarch'
       $service_name         = 'consul'
     }
     default: {
